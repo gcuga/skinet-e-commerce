f1846e41e8c580103d9a3eb8e3b1058b98499dbe
gcuga <git@gcugreyarea.com>
2020-10-26 22:45:13 +0300

    End of section 11

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -7,6 +7,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shop-params';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -44,6 +45,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseURL}products/${id}`);
   }
 
   getBrands(): Observable<IBrand[]> {
